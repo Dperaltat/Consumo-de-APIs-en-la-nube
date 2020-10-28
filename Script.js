5fb3e1fa1268709e287567fd8e58e9f5351a7161
Daniel Peralta <34029227+Dperaltat@users.noreply.github.com>
2020-10-28 01:29:00 -0500

    Falta paginación

--- Script.js
+++ Script.js
@@ -1,21 +1,31 @@
-let buton=document.getElementById('button')
 
-buton.addEventListener('click',()=>{
-    let caja=document.getElementById('caja').value
-    let img=document.getElementById('img')
-    let p=document.getElementById('info')
+    let buton=document.getElementById('button')
 
-    let xhttp=new XMLHttpRequest()
-    xhttp.open("GET",`https://pokeapi.co/api/v2/pokemon/${caja}`)
-    xhttp.send()
+    buton.addEventListener('click',()=>{
+        let caja=document.getElementById('caja').value
+        let img=document.getElementById('img')
+        let p=document.getElementById('info')
+        let p1=document.getElementById('info1')
+        let p2=document.getElementById('info2')
+        let p3=document.getElementById('info3')
+        let p4=document.getElementById('info4')
 
-    xhttp.onreadystatechange=function () {
-        if(this.readyState==4 && this.status==200){
+        let xhttp=new XMLHttpRequest()
+        xhttp.open("GET",`https://pokeapi.co/api/v2/pokemon/${caja}`)
+        xhttp.send()
+
+        xhttp.onreadystatechange=function () {
+            if(this.readyState==4 && this.status==200)
+            {
             let datoPokemon=JSON.parse( this.responseText)
             console.log(datoPokemon)
             img.setAttribute("src",datoPokemon.sprites.front_default)
             p.textContent=datoPokemon.id
+            p1.textContent=datoPokemon.name
+            p2.textContent=datoPokemon.base_experience
+            p3.textContent=datoPokemon.height
+            p4.textContent=datoPokemon.weight
             }
         }
 
-})
+    })
