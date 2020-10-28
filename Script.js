fa3d90ebf7f742fed8d30dd0e8c65ce466bd0276
Daniel Peralta <34029227+Dperaltat@users.noreply.github.com>
2020-10-27 21:59:04 -0500

    Nueva API

---
+++ Script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector('.input');
+const buscar = document.querySelector('.buscar');
+const lista = document.querySelector('.lista');
+
+let url = `https://pokeapi.co/api/v2/pokemon/?q=${topic}`
+
